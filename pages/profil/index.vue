1e93768b3d0f07979bdcb1124d1a577ceb0ba525
DevDams <ad.aimedesire@gmail.com>
2021-08-23 12:25:54 +0000

    start design profil page

--- pages/profil/index.vue
+++ pages/profil/index.vue
@@ -19,12 +19,69 @@
       >
         <h1 class="text-2xl text-myviolet font-extrabold">Profil</h1>
       </div>
+      <div class="profil_info">
+        <div class="profil_image relative w-full h-32">
+          <div class="banner bg-green-300 h-32 flex items-center justify-center">
+            <img src="~assets/svg/picture.svg" alt="picture icon" class="h-12 w-12">
+            <div class="edit_banner"></div>
+          </div>
+          <div class="profil_pic absolute top-16 left-4 bg-indigo-500 h-28 w-28 border-4 border-white rounded-full">
+            <img src="" alt="">
+          </div>
+          <div class="edit_profil absolute top-36 right-4">
+            <button class="border border-gray-300 font-semibold px-4 py-1 rounded-full bg-white">Editer le profil</button>
+          </div>
+        </div>
+        <div class="profil_text w-full h-38 mt-12 px-6">
+          <div>
+            <p class="username text-2xl font-semibold">Aimé-Désiré</p>
+            <p class="pseudo text-md text-gray-500 font-normal">@dams9ix</p>
+            <p class="mt-2 bio">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus placeat commodi delectus tenetur ullam maiores atque voluptatib rovident obcaecati hic.</p>
+            <div class="flex items-center mt-2">
+              <img src="~assets/svg/calendar.svg" alt="calendar icon">
+              <p class="text-gray-500 ml-2">A rejoint MuseX en mars 2021</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="user_posts w-full px-12 pt-12 bg-blue-200">
+        <div class="post flex border">
+          <div class="pp">
+            <div class="w-16 h-16 rounded-full bg-black">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1">
+            <div class="user_info flex items-center">
+              <div class="name font-semibold text-lg">Aimé-Désiré</div>
+              <div class="pseudo ml-3 font-normal text-gray-500">@dams9ix</div>
+            </div>
+            <div class="post_description">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus placeat commodi delectus tenetur ullam maiores atque voluptatib rovident obcaecati hic.</p>
+            </div>
+            <div class="audio">
+              <audio src=""></audio>
+            </div>
+            <div class="post_action">
+              <div class="like">❤ 123</div>
+              <div class="share">🔁 321</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    para () {
+      const bio = document.querySelector('.bio')
+      console.log(bio.innerHTML.length)
+    }
+  },
+}
 </script>
 
 <style scoped>
@@ -50,6 +107,11 @@ export default {}
   border-right: 1px solid #eaeaea;
   border-left: 1px solid #eaeaea;
   margin-left: 256px;
+}
+
+.profil_text {
+  padding-bottom: 25px;
+  border-bottom: 1px solid #e5e7eb;
 }
 
 @media (max-width: 996px) {
