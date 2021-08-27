fcce0aaf93c2cc2eb797524ed5fd26dcb464d459
DevDams <ad.aimedesire@gmail.com>
2021-08-27 13:29:29 +0000

    design audio page and modif post card

--- pages/audios/index.vue
+++ pages/audios/index.vue
@@ -2,7 +2,7 @@
   <div class="home mx-auto flex">
     <Navbar class="desktop" />
     <MobileNavbar class="mobile" />
-    <div class="content_block relative w-full h-screen">
+    <div class="content_block relative w-full h-full">
       <div
         class="
           header
@@ -18,6 +18,321 @@
         "
       >
         <h1 class="text-2xl text-myviolet font-extrabold">Mes audios</h1>
+      </div>
+      <div class="intro-text sticky top-0 px-3 py-3 bg-gray-50 z-30">
+        <p class="text-xl font-medium w-96">
+          Gérez tous vos audios, supprimez-les ou modifiez leur description.
+        </p>
+      </div>
+      <div class="all_post px-6 mt-10">
+        <!-- Post model -->
+        <div class="post relative flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="absolute right-3 top-3 w-9 h-9 flex items-center justify-center bg-white rounded-full border-2 border-gray-100 shadow-md">
+            <button>
+              <img src="~/assets/svg/modif.svg" alt="">
+            </button>
+          </div>
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/trash.svg" alt="share icon" class="h-6 w-6">
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Post model -->
+        <div class="all_post flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Post model -->
+        <div class="all_post flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Post model -->
+        <div class="all_post flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Post model -->
+        <div class="all_post flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- Post model -->
+        <div class="all_post flex border py-3 px-2 mb-24 rounded-lg">
+          <div class="pp">
+            <div class="pp w-16 h-16 rounded-full bg-indigo-500">
+              <img src="" alt="">
+            </div>
+          </div>
+          <div class="content ml-3 mt-1 w-full">
+            <div class="user_info flex flex-col justify-center">
+              <div class="name font-semibold text-lg leading-5">Comme projet comme projet</div>
+              <div class="-mt-1">
+                <span class="pseudo font-normal text-gray-500">@dams9ix</span>
+                <span class="mx-1 text-xl">·</span>
+                <span class="text-gray-500 font-normal">15h</span>
+              </div>
+            </div>
+            <div class="description mt-2">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dicta quod quis est temporibus ex deleniti iusto enim quaerat. Mollitia cum nesciunt vitae qui laudantium eaque culpa tempore tenetur di
+              </p>
+            </div>
+            <div class="audio mt-4 w-full">
+              <div class="music-player w-full h-16 flex items-center shadow-md rounded-xl border-2 border-gray-600 p-4">
+                <div class="controls">
+                  <div class="play-pause -mt-1">
+                    <img src="~/assets/svg/play.svg" alt="">
+                  </div>
+                </div>
+                <div class="progress-area bg-gray-300 h-0.5 rounded-full w-full ml-3 -mt-1">
+                  <div class="progress-bar relative rounded-full"></div>
+                  <div class="timer flex items-center justify-between text-md text-gray-500 mt-1">
+                    <span class="current">0:00</span>
+                    <span class="duration"></span>
+                  </div>
+                  <audio class="main-audio" src=""></audio>
+                </div>
+              </div>
+            </div>
+            <div class="post_action w-11/12 pl-6 flex justify-between mt-4">
+              <button class="like flex items-center">
+                <img src="~assets/svg/heart-white.svg" alt="heart icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+              <button class="like flex items-center">
+                <img src="~assets/svg/share.svg" alt="share icon" class="h-6 w-6">
+                <span class="ml-1 font-medium text-gray-600">24</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -52,6 +367,43 @@ export default {}
   margin-left: 256px;
 }
 
+/* Music player */
+
+.music-player {
+  width: 95%;
+  height: 65px;
+}
+
+.progress-area .progress-bar {
+  height: inherit;
+  width: 0;
+  background: linear-gradient(90deg, #42ACF2 0%,#B042F2 100%);
+}
+
+.progress-area .progress-bar::before {
+  position: absolute;
+  content: '';
+  width: 12px;
+  height: 12px;
+  border-radius: inherit;
+  top: 50%;
+  right: -5px;
+  transform: translateY(-50%);
+  background: inherit;
+  opacity: 0;
+  transition: opacity 0.2s ease;
+}
+
+.progress-area:hover .progress-bar::before {
+  opacity: 1;
+}
+
+.controls .play-pause img {
+  user-select: none;
+  width: 28px;
+  cursor: pointer;
+}
+
 @media (max-width: 996px) {
   .home {
     width: 700px;
@@ -78,6 +430,37 @@ export default {}
   .content_block {
     margin-left: 0;
     width: 100%;
+  }
+}
+
+@media (max-width: 637px) {
+  .post {
+    margin-bottom: 150px;
+  }
+}
+
+@media (max-width: 465px) {
+  .pp {
+    width: 56px;
+    height: 56px;
+  }
+}
+
+@media (max-width: 405px) {
+  .intro-text p {
+    font-size: 17px;
+    width: 80%;
+  }
+}
+
+@media (max-width: 375px) {
+  .all_post {
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+
+  .post {
+    font-size: 14px;
   }
 }
 </style>
