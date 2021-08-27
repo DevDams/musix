6e7380978b9bf684e5622accf89b1da677a7776d
DevDams <ad.aimedesire@gmail.com>
2021-08-27 14:33:59 +0000

    unuse modif

--- components/PostForm.vue
+++ components/PostForm.vue
@@ -3,17 +3,19 @@
     <div class="post">
       <div class="post_form h-64 mx-auto flex flex-col border-2 border-gray-200 shadow-lg mt-20 rounded-xl bg-white z-50">
         <div class="title h-14 w-full px-4 flex items-center justify-end">
-          <p class="add text-right rounded-full px-3 py-1 text-sm font-medium text-white bg-myblue shadow-md cursor-pointer">Ajoutez un nouvel audio</p>
+          <p class="add text-right rounded-full px-3 py-2 text-sm font-medium text-white bg-myblue shadow-md cursor-pointer">Ajoutez un nouvel audio</p>
         </div>
         <div class="form_field px-4 pt-2 flex">
-          <div class="profil_pic w-16 h-16 bg-myviolet rounded-full" @click="para">
-            <img src="" alt="">
+          <div class="pic">
+            <div class="profil_pic w-16 h-16 bg-myviolet rounded-full" @click="para">
+              <img src="" alt="">
+            </div>
           </div>
-          <div class="filed ml-2 mt-6">
+          <div class="field ml-2 mt-6">
             <form action="">
               <div class="description relative">
                 <textarea v-model="content" name="description" id="description" placeholder="Dites quelque chose à propos de cet audio" class="w-96 h-32 p-2 border-2 border-gray-300 rounded-lg"></textarea>
-                <div class="description_length absolute bottom-2 right-6 text-right">
+                <div class="description_length absolute bottom-2 right-3 text-right">
                   <span>{{ content.length }}/150</span>
                 </div>
               </div>
@@ -80,7 +82,7 @@ export default {
 }
 
 .title p.add {
-  background: linear-gradient(120deg, #42ACF2 0%, #B042F2 100%);
+  background-color: black;
 }
 
 textarea {
@@ -98,13 +100,21 @@ textarea:focus {
     width: 90%;
   }
 
+  .pic {
+    width: 15%;
+  }
+
   .profil_pic {
     width: 50px;
     height: 50px;
   }
 
+  .field {
+    width: 85%;
+  }
+
   .description textarea {
-    width: calc(100% - 50px);
+    width: 100%;
   }
 }
 </style>
